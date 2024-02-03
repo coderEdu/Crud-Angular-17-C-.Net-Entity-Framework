9594918a0cb2b8fad29e9f2407ed23c18af38016
coderEdu <gtpanteraxtr@gmail.com>
2024-02-03 17:01:24 -0300

    changes on vid-8

--- src/app/components/agregar-editar-comentario/agregar-editar-comentario.component.ts
+++ src/app/components/agregar-editar-comentario/agregar-editar-comentario.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Comentario } from '../../interfaces/Comentario';
+import { FormsModule, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-editar-comentario',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, FormsModule],
   templateUrl: './agregar-editar-comentario.component.html',
   styleUrl: './agregar-editar-comentario.component.css'
 })
 export class AgregarEditarComentarioComponent {
+  agregarComentario: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.agregarComentario = this.fb.group({
+      titulo: ['', Validators.required],
+      creador: ['', Validators.required],
+      texto: ['', Validators.required],
+    })
+  }
+
+  agregar() {
+    console.log(this.agregarComentario);
+
+    const comentario: Comentario = {
+      titulo: this.agregarComentario.get('titulo')?.value,
+      creador: this.agregarComentario.get('creador')?.value,
+      texto: this.agregarComentario.get('texto')?.value,
+      fechaCreacion: new Date,
+    }
+
+    console.log(comentario);
+  }
 }
