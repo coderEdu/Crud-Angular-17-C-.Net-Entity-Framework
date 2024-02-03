1ff777f77db50440336a18a3e65c206d341132e7
coderEdu <gtpanteraxtr@gmail.com>
2024-02-03 09:29:12 -0300

    changes on vid-7

--- src/app/components/agregar-editar-comentario/agregar-editar-comentario.component.ts
+++ src/app/components/agregar-editar-comentario/agregar-editar-comentario.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-editar-comentario',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './agregar-editar-comentario.component.html',
   styleUrl: './agregar-editar-comentario.component.css'
 })
